1a2a9403b757930854548c8099ec1c362c9c6d54
github_username <email_address>
2025-09-19 04:17:16 -0600

    primer

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Akemi Clarissa Olvera Arao
+19/09/25
+Determinar si un año es bisiesto o no
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+año = int(input("Ingrese un año: "))
 
 # Proceso
+if año % 4 == 0 and año % 100 != 0:
+     print(año, "es un año bisiesto")
+elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0:
+    print(año, "es un año bisiesto")
+elif año % 100 == 0:
+    print(año, "no es un año bisiesto")
 
 
 # Salidas
-print(salida)
+
